a00935c8b43531014b9eb51771c728230dba40de
Benjamin Mampaey <benjamin.mampaey@oma.be>
2015-01-20 17:58:17 +0100

    Corrected dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
 	name = 'SOLARNET',
 	packages = ['SOLARNET'],
-	version = '0.1',
+	version = '0.2.1',
 	description = 'A python client to access the RESTfull interface of the SOLARNET Data Archive',
 	author = 'Benjamin Mampaey',
 	author_email = 'bmampaey@gmail.com',
 	url = 'https://github.com/bmampaey/SOLARNET-python-client',
 	download_url = 'https://github.com/bmampaey/SOLARNET-python-client/tarball/0.1',
-	install_requires=['dateutil', 'slumber', 'pyfits'],
+	install_requires=['python-dateutil', 'slumber', 'pyfits'],
 	keywords = ['sun', 'astronomy', 'SOLARNET', 'RESTfull'],
 	classifiers = ['Intended Audience :: Science/Research', 'Development Status :: 4 - Beta', 'License :: OSI Approved :: MIT License','Natural Language :: English', 'Operating System :: POSIX :: Linux', 'Programming Language :: Python :: 2',  'Topic :: Scientific/Engineering :: Astronomy'],
+	package_data = {'': ['*.txt', '*.rst', '*.md']}
 )
